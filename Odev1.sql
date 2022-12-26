0db1b08cda403e42d47162aa8332c1d536992337
Furkan Çetin NAMLI <71931667+Fzxselfcn@users.noreply.github.com>
2022-12-26 18:40:28 +0300

    Update and rename Odev1.txt to Odev1.sql

--- Odev1.sql
+++ Odev1.sql
@@ -1,25 +1,25 @@
-1.Sorgu:
+--1.Sorgu:
 
 SELECT title, description FROM film;
 
-2.Sorgu:
+--2.Sorgu:
 
 SELECT * FROM film
 WHERE lenght > 60 AND length < 75;
 
-3.Sorgu:
+--3.Sorgu:
 
 SELECT * FROM film
 WHERE rental_rate = 0.99 AND replacement_cost = 12.99 OR replacement_rate = 28.99;
 
-4.Sorgu:
+--4.Sorgu:
 
 SELECT last_name FROM customer
 WHERE first_name = 'Mary';
 
-(cevap = 'Sm,th')
+--(cevap = 'Sm,th')
 
-5.Sorgu:
+--5.Sorgu:
 
 SELECT * FROM film
 WHERE (NOT length > 50) AND (NOT (rental_rate = 2.99 OR rental_rate = 4.99));
