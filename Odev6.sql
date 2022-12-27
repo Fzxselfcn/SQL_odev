91de6045d2e2a80acdf2214d10c445e1028442de
Furkan Çetin NAMLI <71931667+Fzxselfcn@users.noreply.github.com>
2022-12-27 21:28:13 +0300

    Create Odev6.sql

---
+++ Odev6.sql
@@ -0,0 +1,21 @@
+--1.Sorgu:
+
+SELECT AVG(rental_rate) FROM film;
+
+
+--2.Sorgu
+
+SELECT COUNT(*) FROM film
+WHERE title LIKE 'C%';
+
+
+--3.Sorgu
+
+SELECT MAX(length) FROM film
+WHERE rental_rate = 0.99;
+
+
+--4.Sorgu
+
+SELECT COUNT(DISTINCT replacement_cost) FROM film
+WHERE length > 50;
